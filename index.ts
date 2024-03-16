e784038ce3c78fb435ae36d334fa8c95e33402b8
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2024-03-15 22:31:36 -0400

    add colors

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
+import chalk from "chalk";
 import tmi from "tmi.js";
+
+const colors = ["blue", "red", "yellow", "green", "magenta", "cyan"] as const;
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
 
 const client = new tmi.Client({
   channels: ["ss_dvd"],
@@ -9,5 +16,7 @@ const client = new tmi.Client({
 client.connect();
 
 client.on("message", (channel: any, tags: any, message: any, self: any) => {
-  console.log(`${tags["display-name"]}: ${message}`);
+  console.log(
+    chalk[getRandomColor()](`${tags["display-name"]}`) + `: ${message}`,
+  );
 });
